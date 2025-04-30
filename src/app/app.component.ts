8779482cc3ffbab9128c679bcec0579b81c10cc0
Bruno Stival Bontorin <brunostival@lugarh.com.br>
2025-04-30 16:04:09 -0300

    Edit - Footer setup, custom screen for very small screen size and adding more color variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  selector: 'root',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
-    <header-layout />
-    <router-outlet />
+    <section class="layout">
+      <header-layout />
+      <main class="layout__main">
+        <router-outlet />
+      </main>
+      <footer-layout />
+    </section>
   `,
 })
 export class AppComponent {}
---
+++ src/app/layouts/footer/footer.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer-layout',
+  imports: [],
+  template: `
+    <footer class="footer">
+      <div class="footer__content">
+        <span class="footer__text">© 2025 Todos os direitos reservados - Desenvolvido por bsbontorin</span>
+      </div>
+    </footer>
+  `,
+  styles: `
+    @use 'mixins' as *;
+    @use 'variables' as *;
+
+    .footer {
+      color: var(--white);
+      background-color: var(--gray-700);
+      margin: auto 2rem;
+      padding: 0.5rem 1rem;
+      font-size: 0.75rem;
+      text-align: center;
+      border-radius: 0.5rem;
+      margin-bottom: 1rem;
+
+      &__content {
+        @include flexbox(column, center, center);
+        flex-wrap: wrap;
+      }
+    }
+  `,
+})
+export class FooterComponent {}
